b9017d49f162f596e1d03d0b197dc2b7bc99af2e
=Historical <=>
2015-04-15 21:25:37 -0500

    added Point.py

--- Point.py
+++ Point.py
@@ -1,12 +1,10 @@
+class Point(object):
+	"""docstring for Point"""
+	def __init__(self, arg):
+		super(Point, self).__init__()
+		self.arg = arg
 
-
-a
-a
-a
-a
-asdf
-asdfasdf
-
-
-
-kalkdsf
+	def __str__(self):
+		""""""
+		return 'this string'
+		
