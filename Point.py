074c6e6127a345b28b40fc33b13968c846ff1996
=Historical <mikemommsen@gmail.com>
2015-04-15 22:19:11 -0500

    added add function

--- Point.py
+++ Point.py
@@ -7,4 +7,9 @@ class Point(object):
 
 	def __str__(self):
 		""""""
-		return 'this string'
+		return 'point object at ({}, {})'.format(self.x, self.y)
+
+	def __add__(self, other):
+		""""""
+		return self.x + other.x, self.y + other.y
+		
